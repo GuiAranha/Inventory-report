4f8316f15b1af35e5e3d42ce96b0156157e3149c
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-10-31 10:13:34 -0300

    feat: create tests for product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "30-10-2022",
+        "31-10-2022",
+        "numero_de_serie",
+        "instrucoes_de_armazenamento",
+    )
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
